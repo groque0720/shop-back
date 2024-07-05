f67b6856dd25822803e79ad491ffd294c3a6e48e
Gómez Roque <gomezroque@hotmail.com>
2024-07-05 09:54:49 -0300

    first commit

---
+++ applications/users/api/router.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from applications.users.api.views import RegisterView, UserView
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/me/', UserView.as_view(), name='me'),
+]
---
+++ applications/users/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from applications.users.models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    # encriptación de la contraseña al crear un usuario 
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password) 
+        instance.save()
+        return instance
+    
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email','first_name','last_name']
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
