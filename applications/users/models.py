86a5a0a43b5e7c6ab54d2991936d7164502f08d9
Gómez Roque <gomezroque@hotmail.com>
2024-07-08 16:51:14 -0300

    Usercreate

--- applications/users/models.py
+++ applications/users/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    pass
+    # email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = []
